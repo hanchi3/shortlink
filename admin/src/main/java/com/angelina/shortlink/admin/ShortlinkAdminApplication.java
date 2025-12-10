2f16d43004b09213ebc41a0be53e2e92d4a3715d
hanchi3 <1991595662@qq.com>
2025-12-10 23:48:34 +0800

    初始化目录

---
+++ admin/src/main/java/com/angelina/shortlink/admin/ShortlinkAdminApplication.java
@@ -0,0 +1,12 @@
+package com.angelina.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortlinkAdminApplication.class, args);
+    }
+}
