80a8fcd8eb093f060d736fd0e165082b9b017dd3
hanchi3 <1991595662@qq.com>
2025-12-29 23:36:42 +0800

    feature: 开发短链接后管系统分组crud功能

---
+++ admin/src/main/java/com/angelina/shortlink/admin/service/GroupService.java
@@ -0,0 +1,47 @@
+package com.angelina.shortlink.admin.service;
+
+import com.angelina.shortlink.admin.dao.entity.GroupDO;
+import com.angelina.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
+import com.angelina.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
+import com.angelina.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+public interface GroupService extends IService<GroupDO> {
+
+    /**
+     * 新增短链接分组
+     *
+     * @param groupName 短链接分组名
+     */
+    void saveGroup(String groupName);
+
+    /**
+     * 查询用户短链接分组集合
+     *
+     * @return 用户短链接分组集合
+     */
+    List<ShortLinkGroupRespDTO> listGroup();
+
+    /**
+     * 修改短链接分组
+     *
+     * @param requestParam 修改链接分组参数
+     */
+    void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
+
+    /**
+     * 删除短链接分组
+     *
+     * @param gid 短链接分组标识
+     */
+    void deleteGroup(String gid);
+
+    /**
+     * 短链接分组排序
+     *
+     * @param requestParam 短链接分组排序参数
+     */
+    void sortGroup(List<ShortLinkGroupSortReqDTO> requestParam);
+}
---
+++ admin/src/main/java/com/angelina/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,97 @@
+package com.angelina.shortlink.admin.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.angelina.shortlink.admin.common.biz.user.UserContext;
+import com.angelina.shortlink.admin.dao.entity.GroupDO;
+import com.angelina.shortlink.admin.dao.mapper.GroupMapper;
+import com.angelina.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
+import com.angelina.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
+import com.angelina.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
+import com.angelina.shortlink.admin.service.GroupService;
+import com.angelina.shortlink.admin.toolkit.RandomGenerator;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .sortOrder(0)
+                .username(UserContext.getUsername())
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
+    }
+
+
+    @Override
+    public void updateGroup(ShortLinkGroupUpdateReqDTO requestParam) {
+        LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, requestParam.getGid())
+                .eq(GroupDO::getDelFlag, 0);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setName(requestParam.getName());
+        baseMapper.update(groupDO, updateWrapper);
+    }
+
+    @Override
+    public void deleteGroup(String gid) {
+        LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, gid)
+                .eq(GroupDO::getDelFlag, 0);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setDelFlag(1);
+        baseMapper.update(groupDO, updateWrapper);
+    }
+
+    @Override
+    public void sortGroup(List<ShortLinkGroupSortReqDTO> requestParam) {
+        requestParam.forEach(each -> {
+            GroupDO groupDO = GroupDO.builder()
+                    .sortOrder(each.getSortOrder())
+                    .build();
+            LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                    .eq(GroupDO::getUsername, UserContext.getUsername())
+                    .eq(GroupDO::getGid, each.getGid())
+                    .eq(GroupDO::getDelFlag, 0);
+            baseMapper.update(groupDO, updateWrapper);
+        });
+    }
+
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                .eq(GroupDO::getUsername, UserContext.getUsername());
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
+}
